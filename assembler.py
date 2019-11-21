993ea5be7e16272ab96d72d2ed2eafee145b4602
nick calderone <calderonenick@gmail.com>
2019-11-21 15:07:51 -0700

    cleaned up assembler

--- assembler.py
+++ assembler.py
@@ -1,18 +1,22 @@
+#!/usr/bin/env python3
+
+import sys
+
 from get_lines import get_lines
 from ignore_comment import ignore_comment
 from ignore_white import ignore_white
 from ignore_blank_lines import ignore_blank_lines
-from converter import converter
+from converter import converter 
 
-with open('nand2tetrisfiles/projects/06/max/MaxL.asm', 'r') as f:
+file = sys.argv[1]
+
+with open(f'/home/nick/PycharmProjects/coursera/nand2tetris/assembler/{file}', 'r') as f:
     # clean up program lines
     full_program = get_lines(f)
     clean_program = ignore_comment(ignore_blank_lines(ignore_white(full_program)))
-    # print(clean_program)
 
     # a operation/c operation handler
     value = converter(clean_program)
-    print(value)
 
     with open('result.hack', 'w') as r:
         for item in value:
